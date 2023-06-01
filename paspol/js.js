b4721419f6d278edb11140365c11a1e066660aa0
misan <nojikenn@hotmail.com>
2023-06-01 11:46:44 +0900

    あと少しで半分

---
+++ paspol/js.js
@@ -0,0 +1,7 @@
+document.getElementById('upScroll')
+
+.addEventListener('click', () => {
+    var a = document.documentElement;
+    var y = a.scrollHeight - a.clientHeight;
+    window.scroll({top: 0});
+  });
